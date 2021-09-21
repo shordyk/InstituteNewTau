5474a52d74683b5441dc7668e3f3c4d3d6f91500
Paul Summers <psummers@stanford.edu>
2021-09-21 16:02:19 -0700

    load ISSM and ELMER and PISM

--- ISSM/LoadinitISMIP.m
+++ ISSM/LoadinitISMIP.m
@@ -1,45 +1,102 @@
 clear;
 % https://tc.copernicus.org/articles/13/1441/2019/tc-13-1441-2019.pdf
+%%
+% filename = "JPL1_ISSM_ctrl/strbasemag_AIS_JPL1_ISSM_ctrl.nc";
+% filename_init = "JPL1_ISSM_init/strbasemag_AIS_JPL1_ISSM_init.nc";
 
+% filename = "ARC_PISM1_ctrl/strbasemag_AIS_ARC_PISM1_ctrl.nc";
+filename2 = "IGE_ELMER_ctrl/strbasemag_AIS_IGE_ELMER_ctrl.nc";
+
+% 
+filename = "JPL1_ISSM_ctrl/strbasemag_AIS_JPL1_ISSM_ctrl.nc";
 x   = ncread("JPL1_ISSM_init/strbasemag_AIS_JPL1_ISSM_init.nc","x");
 y   = ncread("JPL1_ISSM_init/strbasemag_AIS_JPL1_ISSM_init.nc","y");
-tau = ncread("JPL1_ISSM_init/strbasemag_AIS_JPL1_ISSM_init.nc","strbasemag");
-tau2 = ncread("JPL1_ISSM_ctrl/strbasemag_AIS_JPL1_ISSM_ctrl.nc","strbasemag");
-x2   = ncread("JPL1_ISSM_ctrl/strbasemag_AIS_JPL1_ISSM_ctrl.nc","x");
+tau = ncread("JPL1_ISSM_ctrl/strbasemag_AIS_JPL1_ISSM_ctrl.nc","strbasemag");
+% x   = ncread(filename,"x");
+% y   = ncread(filename,"y");
+% tau = ncread(filename,"strbasemag");
 
+tau2 = ncread(filename2,"strbasemag");
+x2   = ncread(filename2,"x");
+y2   = ncread(filename2,"y");
 
+%% Clean some values
 x = x - 3072000;
 y = y - 3072000;
+x2 = x2;
+y2 = y2;
+tau(isnan(tau)) = 0;
+tau2(isnan(tau2)) = 0;
 
+%%
 [xx,yy] = ndgrid(x,y);
-uB = griddedInterpolant(xx,yy,tau');
-uB2 = griddedInterpolant(xx,yy,tau2(:,:,21)');
+[xx2,yy2] = ndgrid(x2,y2);
+uB = griddedInterpolant(xx,yy,tau(:,:,21));
+uB2 = griddedInterpolant(xx2,yy2,tau2(:,:,21));
 
-figure(1)
-clf
-surf(x,y,tau'/1e3,'edgecolor','none')
-caxis([0 150]);
-colorbar
-view(2)
-
+% figure(1)
+% clf
+% surf(x,y,tau'/1e3,'edgecolor','none')
+% caxis([0 150]);
+% colorbar
+% view(2)
+% 
 x0 = mean(x);
 y0 = mean(y);
 
-figure(2)
+figure(1)
 clf
-surf(x,y,uB(xx,yy)/1e3,'edgecolor','none')
+surf(x,y,uB(xx,yy)'/1e3,'edgecolor','none')
 hold on
 scatter3(x0,y0,400,[],'k','filled');
 caxis([0 150]);
 colorbar
 view(2)
+tmp1 = split(filename,"/");
+title(tmp1(1))
 
+%%
+figure(2)
+clf
+surf(x2,y2,uB2(xx,yy)'/1e3,'edgecolor','none')
+hold on
+caxis([0 150]);
+colorbar
+view(2)
+tmp2 = split(filename2,"/");
+title(tmp2(1))
 %%
 figure(3)
 clf
-surf(x,y,(uB(xx,yy) - uB2(xx,yy))/1e3,'edgecolor','none')
+surf(x2,y2,(uB(xx,yy) - uB2(xx,yy))'/1e3,'edgecolor','none')
 hold on
-scatter3(x0,y0,400,[],'k','filled');
-% caxis([0 150]);
+caxis([-150 150]);
 colorbar
+colormap redblue
+title(tmp1(1) + " - " + tmp2(1))
 view(2)
+%%
+load ../gridInstitute24000.mat
+figure(4)
+clf
+subplot(131)
+trisurf(t,xy(:,1),xy(:,2),uB(xy(:,1),xy(:,2))/1e3,...
+       'edgecolor','none')
+title(tmp1(1))
+view(2)
+colorbar
+
+subplot(132)
+trisurf(t,xy(:,1),xy(:,2),uB2(xy(:,1),xy(:,2))/1e3,...
+       'edgecolor','none')
+title(tmp2(1))
+view(2)
+colorbar
+
+ax(3) = subplot(133);
+trisurf(t,xy(:,1),xy(:,2),(uB(xy(:,1),xy(:,2)) - uB2(xy(:,1),xy(:,2)))/1e3,...
+       'edgecolor','none')
+colormap(ax(3), redblue)
+title(tmp1(1) + " - " + tmp2(1))
+view(2)
+colorbar
---
+++ lib/velocityProfiles3.m
@@ -0,0 +1,94 @@
+clear;
+
+[seed_lat, seed_lon] = ps2ll(-9.2212e5,2.5977e5);
+% gen_vel_profiles(seed_lat, seed_lon,5);
+
+load institute_antiflow/vel_profile_full.mat
+figure
+measures('speed','institute ice stream','scalelim',[1 600],'mapwidth',800);
+plotm(profile_lat,profile_lon,'k','LineWidth',5)
+plotm(seed_lat, seed_lon,'rp')
+figure
+for j = 1:size(profile_path,2)
+    subplot(2,size(profile_path,2),j)
+    plot(profile_path(:,j)./1e3,profile_cross(:,j),profile_path(:,j)./1e3,profile_along(:,j),'LineWidth',3)
+    ylabel('Speed [m/yr]')
+    legend('Across-track','Along-track','Location','NW')
+    ylim([-100 250])
+    xlim([profile_path(1,j)./1e3 profile_path(end,j)./1e3])
+    subplot(2,size(profile_path,2),size(profile_path,2)+j)
+    [hice, hbed, hwater] = bedmap2_profile(profile_lat(:,j),profile_lon(:,j));
+    ylabel('Elevation [m]')
+    xlabel('Along-track Distance [km]')
+end
+%%
+load vel_profiles_paul.mat
+figure
+mapzoomps('Institute Ice Stream')
+measuresps('speed')
+% [x,y,c] = measures_data('speed',xlim,ylim,'xy');
+% h = pcolor(x,y,c);
+% set(h, 'EdgeColor', 'none');
+plotps(profile_lat,profile_lon,'k','LineWidth',5)
+hold on
+% plotm(seed_lat, seed_lon)
+bedmachine('surface','contour',0:100:5000,'k')
+caxis([1 1200]);
+set(gca,'ColorScale','log')
+colorbar
+%%
+figure
+for j = 1:size(profile_path,2)
+    subplot(2,size(profile_path,2),j)
+    plot(profile_path(:,j)./1e3,profile_cross(:,j),profile_path(:,j)./1e3,profile_along(:,j),'LineWidth',3)
+    ylabel('Speed [m/yr]')
+    legend('Across-track','Along-track','Location','NW')
+    ylim([-100 250])
+    xlim([profile_path(1,j)./1e3 profile_path(end,j)./1e3])
+    subplot(2,size(profile_path,2),size(profile_path,2)+j)
+    [hice, hbed, hwater] = bedmap2_profile(profile_lat(:,j),profile_lon(:,j));
+    ylabel('Elevation [m]')
+    xlabel('Along-track Distance [km]')
+end
+
+
+
+function gen_vel_profiles(seed_lat_in, seed_lon_in,n)
+
+    seed_lat = seed_lat_in*ones(1,n)-.2*[0:n-1];
+    seed_lon = seed_lon_in*ones(1,n)-.2*[0:n-1];
+    for j = 1:length(seed_lat)
+        profile_lat_temp = [seed_lat(j)];
+        profile_lon_temp = [seed_lon(j)];
+
+        for i = 1:90 %50 previously
+            [x1,y1] = ll2ps(profile_lat_temp(1),profile_lon_temp(1));
+            vx1 = measures_interp('vx',profile_lat_temp(1),profile_lon_temp(1));
+            vy1 = measures_interp('vy',profile_lat_temp(1),profile_lon_temp(1));
+            v1 = sqrt(vx1^2 + vy1^2);
+            [x2,y2] = ll2ps(profile_lat_temp(end),profile_lon_temp(end));
+            vx2 = measures_interp('vx',profile_lat_temp(end),profile_lon_temp(end));
+            vy2 = measures_interp('vy',profile_lat_temp(end),profile_lon_temp(end));
+            v2 = sqrt(vx2^2 + vy2^2);
+            
+            stepsize = 800;
+            x_temp1 = x1 + stepsize*-(vy1/v1);
+            y_temp1 = y1 + stepsize*(vx1/v1);
+            [lat_temp1,lon_temp1] = ps2ll(x_temp1,y_temp1);
+            x_temp2 = x2 + stepsize*(vy2/v2);
+            y_temp2 = y2 + stepsize*-(vx2/v2);
+            [lat_temp2,lon_temp2] = ps2ll(x_temp2,y_temp2);
+
+            profile_lat_temp = [lat_temp1,profile_lat_temp,lat_temp2];
+            profile_lon_temp = [lon_temp1,profile_lon_temp,lon_temp2];
+        end
+
+        profile_lat(:,j) = profile_lat_temp;
+        profile_lon(:,j) = profile_lon_temp;
+        profile_cross(:,j) = -1*measures_interp('cross',profile_lat_temp,profile_lon_temp);
+        profile_along(:,j) = measures_interp('along',profile_lat_temp,profile_lon_temp);
+        profile_path(:,j) = pathdist(profile_lat_temp,profile_lon_temp);
+    end
+    save('vel_profiles_paul.mat','profile_along','profile_cross','profile_lat',...
+                            'profile_lon','profile_path')
+end
