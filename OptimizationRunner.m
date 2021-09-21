d06932e56162c3414aa72e495e09c4880a287f0a
Paul Summers <psummers@stanford.edu>
2021-09-21 16:03:10 -0700

    optimizing a few cases for map view

--- OptimizationRunner.m
+++ OptimizationRunner.m
@@ -1,10 +1,10 @@
 clear; clc; close all
 
-x0 = [1.2];
-str = "ISSM_center";
+x0 = [60e3,120e3];
+str = "Depth_center";
 
 fun = @(x)modelOpt(x,str);
-options = optimset('Display','iter','PlotFcns',@optimplotfval,'TolX',1e-2    ,'TolFun',1e6);	
+options = optimset('Display','iter','PlotFcns',@optimplotfval,'TolX',1e3    ,'TolFun',1e6);	
 [x,fval,exitflag,output] = fminsearch(fun,x0,options) 
 
-save("optOutput_"+ str,'x','str'); 
+save("optOutput_"+ str); 
