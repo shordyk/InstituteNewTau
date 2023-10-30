27a9047aa5fbcb9128502d89d6634e41a89b0fc9
shordyk <130109929+shordyk@users.noreply.github.com>
2023-10-29 17:22:58 -0700

    Added plots for e eff_x

--- speedTroubleShoot.m
+++ speedTroubleShoot.m
@@ -1,24 +1,27 @@
 % Trouble shooting the model results and observational speed
+% Currently, want to trouble shoot grid formation. Make sure speeds match
+% and tau looks right 
+
+%% Observational
+
+overgrab = 0;
+xmax = -7e5;
+xmin = -12e5;
+ymax =  5e5;
+ymin =  -1e5;
 
 
-% Observational
 dx2 = 2e3;
 smth = 4e3;
 oxi = xmin-dx2*overgrab:dx2:xmax+dx2*overgrab;
 oyi = ymin-dx2*overgrab:dx2:ymax+dx2*overgrab;
 [oXi,oYi] = meshgrid(oxi,oyi);
 
-
 b_raw =  bedmachine_interp('bed',oXi,oYi);
 sf_raw =  bedmachine_interp('surface',oXi,oYi);
-phi_raw = rho/rho_w.*sf_raw + (rho_w-rho)/rho_w.*b_raw;
+%phi_raw = rho/rho_w.*sf_raw + (rho_w-rho)/rho_w.*b_raw;
 [uO, vO] = measures_interp('velocity',oXi,oYi);
 spd2  = measures_interp('speed',oXi,oYi);
-overgrab = 0;
-xmax = -7e5;
-xmin = -12e5;
-ymax =  5e5;
-ymin =  -1e5;
 ss2   = zeros(size(uO));
 
 [oux ,  ouy] = gradient(uO,dx2,dx2);
@@ -27,26 +30,26 @@ ss2   = zeros(size(uO));
 oe_eff = sqrt(.5*(oux.^2 + ovy.^2) + (.5*(ouy + ovx)).^2);
 [oe_effx, oe_effy] = gradient(oe_eff.^(1/3-1),dx2,dx2);
 
-% Model speeds
+%% Model speeds
 % Get u,v and xy
 load data_strainMesh035ISSM_centerPaulsBase.mat
 dx = 259.6;
 dy = 252.23 ;
 xi = min(xy(:,1))-dx:dx:max(xy(:,1))+dx;
-yi = (min(xy(:,2))-dy:dy:max(xy(:,2))+dy)';
+yi = (min(xy(:,2))-dy:dy:max(xy(:,2))+dy);
 %xi = xy(:,1);
 %yi = xy(:,2);
 [xxx,yyy] = ndgrid(xi,yi);
 [Xi,Yi] = meshgrid(xi,yi);
 
-us = scatteredInterpolant(xy(:,1),xy(:,2),u);
-vs = scatteredInterpolant(xy(:,1),xy(:,2),v);
+us = scatteredInterpolant(xy(:,1),xy(:,2),u, 'linear', 'none');
+vs = scatteredInterpolant(xy(:,1),xy(:,2),v, 'linear', 'none');
 
 
 vv = vs(xxx,yyy)*3.154E7;
 uu = us(xxx,yyy)*3.154E7;
 ss   = zeros(size(uu));
-spd = sqrt(uu.^2 + vv.^2);;
+spd = sqrt(uu.^2 + vv.^2);
 
 [ux ,  uy] = gradient(uu,dx,dx);
 [vx ,  vy] = gradient(vv,dx,dx);
@@ -99,3 +102,86 @@ axis equal
 setFontSize(16);
 c = colorbar;
 %c.Label.String = 'Speed [m/yr]';
+
+figure
+p = surf(xxx,yyy,zeros(size(uu)),e_effx);
+hold on 
+title('model e_effx')
+set(p, 'edgecolor', 'none');
+view(2)
+axis equal
+setFontSize(16);
+c = colorbar;
+
+
+figure
+p = surf(oXi,oYi,zeros(size(ss2)),oe_effx);
+hold on 
+title('obsv e_effx')
+set(p, 'edgecolor', 'none');
+view(2)
+axis equal
+setFontSize(16);
+c = colorbar;
+%% Debugging Grid Formulation with Tau 
+
+xii   = ncread("~/Documents/MATLAB/ISSM/JPL1_ISSM_init/strbasemag_AIS_JPL1_ISSM_init.nc","x");
+yii   = ncread("~/Documents/MATLAB/ISSM/JPL1_ISSM_init/strbasemag_AIS_JPL1_ISSM_init.nc","y");
+[Xii, Yii] = ndgrid(xii-307200, yii-307200); 
+
+% xi and yi form xxx and yyy, make square grid
+tau_c = defineTau("ISSM_center");
+newtau = tau_c(xy(:,1),xy(:,2),u,v)./norms([u,v],2,2);
+tau_interp = scatteredInterpolant(xy(:,1),xy(:,2),newtau);
+
+% from visualize tau
+xrange = [-1000000 -825000];
+yrange = [ 170000   340000];
+
+x_small = linspace(xrange(1),xrange(2),1000);
+y_small = linspace(yrange(1),yrange(2),1000);
+[X_small,Y_small] = ndgrid(x_small,y_small);
+spd = measures_interp('speed',X_small,Y_small);
+
+figure(2)
+surf(X_small,Y_small,zeros(size(X_small)),tau_interp(X_small,Y_small),'edgecolor','none')
+hold on
+contour(X_small,Y_small,spd,[10,10],'k:')
+contour(X_small,Y_small,spd,[30,30],'k--')
+contour(X_small,Y_small,spd,[100,300],'k-')
+contour(X_small,Y_small,spd,[1000,1000],'k-','linewidth',2)
+colorbar
+title('Basal Strength')
+view(2)
+
+
+
+
+figure
+p = surf(xxx,yyy,zeros(size(ss)),tau_interp(xxx, yyy), "EdgeColor", "none");
+hold on
+%contour(xi,yi,spd, [30, 30] , 'k--','HandleVisibility','off')
+%contour(xi,yi,spd, [100, 300, 3000] , 'k-','HandleVisibility','off')
+%contour(xi,yi,spd, [1000, 1000] , 'k-','LineWidth',2)
+bedmachine('gl','color',rgb('gray'),'linewidth',2)
+title('Tau - ISSM Center')
+colorbar
+view(2)
+
+[XX,YY] = ndgrid(xy(:,1),xy(:,2));
+
+figure
+p = surf(xxx,yyy,zeros(size(ss)),tau_interp(XX, YY), "EdgeColor", "none");
+hold on
+%contour(xi,yi,spd, [30, 30] , 'k--','HandleVisibility','off')
+%contour(xi,yi,spd, [100, 300, 3000] , 'k-','HandleVisibility','off')
+%contour(xi,yi,spd, [1000, 1000] , 'k-','LineWidth',2)
+bedmachine('gl','color',rgb('gray'),'linewidth',2)
+title('Tau - ISSM Center XX')
+colorbar
+view(2)
+
+
+
+
+
